de57f24ed0680730155649f2c56eed8d007792a0
Wilkinson Saturnino <wilkinson.wkn@gmail.com>
2021-05-14 11:51:38 -0300

    Criação de uma classe de teste - 'Vocacional.java'.

---
+++ src/main/java/com/estudos/jobtestes/executaveis/Vocacional.java
@@ -0,0 +1,13 @@
+package com.estudos.jobtestes.executaveis;
+
+public class Vocacional {
+
+	public static void main(String args[]) {
+		int[] ano = {2019, 2020, 2021, 2022, 2023};
+		for(int i=0; i<ano.length; i++) {
+			if(ano[i] == 2021)
+				System.out.println("\nVocacional Shalom " + ano[i] + ". Vence logo, Senhor! Faça-se em mim a tua vontade!");
+		}
+	}
+	
+}
